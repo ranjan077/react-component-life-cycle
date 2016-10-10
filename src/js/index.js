b50e114d9a2c2bb8b40fedc71a4943bfa1532158
Ranjan Nagaraju <rnaga8@sapient.com>
2016-10-10 11:15:28 +0530

    added files

--- src/js/index.js
+++ src/js/index.js
@@ -57,7 +57,7 @@ class MainComponent extends React.Component {
 				<h1>Main component:  {this.state.text}</h1>
 				
 				<SecondComponent secondCompStates ={this.state.text}/>
-				<SecondComponent secondCompStates ={this.state.text}/>
+				<ThirdComponent thirdCompStates ={this.state.text}/>
 			</div>);
 	}
 }
@@ -91,14 +91,51 @@ class SecondComponent extends React.Component {
 	componentWillUnmount() {
 		console.log('cycle::SecondComponent componentWillUnmount');
 	}
+	
 	render() {
 		console.log('cycle::SecondComponent render');
 		return (<div className='second-component' ref='second'>
-				<h2>Child component: {this.props.secondCompStates}</h2>
-
+				<h2>First Child component: {this.props.secondCompStates}</h2>
 			</div>);
 	}
 }
 
-
+class ThirdComponent extends React.Component {
+	componentWillReceiveProps(nextProps) {
+		console.log('cycle::ThirdComponent componentWillReceiveProps '+ nextProps.thirdCompStates);
+	}
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('cycle::ThirdComponent shouldComponentUpdate '+ nextProps);
+		return true;
+	}
+	componentWillUpdate() {
+		console.log('cycle::ThirdComponent componentWillUpdate');
+		
+	}
+	componentWillMount () {
+		console.log('cycle::ThirdComponent componentWillMount');
+		
+		
+	}
+	componentDidUpdate () {
+		console.log('cycle::ThirdComponent componentDidUpdate');
+		
+	}
+	componentDidMount(){
+		console.log('cycle::ThirdComponent componentDidMount');
+		let thirdDom = ReactDom.findDOMNode(this);
+		// console.log(thirdDom);
+		
+	}
+	componentWillUnmount() {
+		console.log('cycle::ThirdComponent componentWillUnmount');
+	}
+	
+	render() {
+		console.log('cycle::ThirdComponent render');
+		return (<div className='second-component' ref='third'>
+				<h2>Second Child component: {this.props.thirdCompStates}</h2>
+			</div>);
+	}
+}
 ReactDom.render(<MainComponent />, document.getElementById('reactApp'));
