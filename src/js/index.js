4b4cf0a90996714ef5b2dc0563bb10a86baaee90
Ranjan Nagaraju <rnaga8@sapient.com>
2016-10-10 10:52:42 +0530

    added files

---
+++ src/js/index.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+require('../css/secondComponet.scss');
+require('../css/mainComponent.scss');
+class MainComponent extends React.Component {
+	static defaultProps() {
+		console.log('cycle::MianComponent getDefaultProps');
+		return {
+			text: 'ranjan'
+		}
+	}
+	constructor() {
+		console.log('cycle::MianComponent getInitialState');
+		super();
+		this.state = {
+			text: MainComponent.defaultProps()
+		}
+	}
+	componentWillReceiveProps(nextProps) {
+		console.log('cycle::MianComponent componentWillReceiveProps '+ nextProps);
+	}
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('cycle::MianComponent shouldComponentUpdate '+ nextProps);
+		return true;
+	}
+	componentWillUpdate() {
+		console.log('cycle::MianComponent componentWillUpdate');
+		
+	}
+	componentWillMount () {
+		console.log('cycle::MianComponent componentWillMount');
+		this.state.text = 'Hello changed';
+		setTimeout(() => {
+			this.setState({
+			text: 'Hello changed again'
+			})
+		}, 3000)
+	}
+	componentDidUpdate () {
+		console.log('cycle::MianComponent componentDidUpdate');
+		this.state.text = 'Hello changed';
+		let mainDom = ReactDom.findDOMNode(this);
+		// console.log(mainDom);
+	}
+	componentDidMount(){
+		console.log('cycle::MianComponent componentDidMount');
+		let mainDom = ReactDom.findDOMNode(this);
+		// console.log(mainDom);
+		
+	}
+	componentWillUnmount() {
+		console.log('cycle::MianComponent componentWillUnmount');
+	}
+	render() {
+		console.log('cycle::MianComponent render');
+		return (<div className='main-component' ref='main'>
+				<h1>Main component:  {this.state.text}</h1>
+				
+				<SecondComponent secondCompStates ={this.state.text}/>
+				<SecondComponent secondCompStates ={this.state.text}/>
+			</div>);
+	}
+}
+class SecondComponent extends React.Component {
+	componentWillReceiveProps(nextProps) {
+		console.log('cycle::SecondComponent componentWillReceiveProps '+ nextProps.secondCompStates);
+	}
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('cycle::SecondComponent shouldComponentUpdate '+ nextProps);
+		return true;
+	}
+	componentWillUpdate() {
+		console.log('cycle::SecondComponent componentWillUpdate');
+		
+	}
+	componentWillMount () {
+		console.log('cycle::SecondComponent componentWillMount');
+		
+		
+	}
+	componentDidUpdate () {
+		console.log('cycle::SecondComponent componentDidUpdate');
+		
+	}
+	componentDidMount(){
+		console.log('cycle::SecondComponent componentDidMount');
+		let secondDom = ReactDom.findDOMNode(this);
+		// console.log(secondDom);
+		
+	}
+	componentWillUnmount() {
+		console.log('cycle::SecondComponent componentWillUnmount');
+	}
+	render() {
+		console.log('cycle::SecondComponent render');
+		return (<div className='second-component' ref='second'>
+				<h2>Child component: {this.props.secondCompStates}</h2>
+
+			</div>);
+	}
+}
+
+
+ReactDom.render(<MainComponent />, document.getElementById('reactApp'));
